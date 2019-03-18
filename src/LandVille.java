c364d7d275ce40560067bf55862c2941a412deb7
Sam <samuel-king@live.com.au>
2019-03-18 16:54:07 +1100

    Added skeleton code

--- src/LandVille.java
+++ src/LandVille.java
@@ -1,4 +1,50 @@
+// import whatever packages are needed here
 
-public class LandVille {
+// Programming note: this class does NOT need to do any input
+// at all. All the input should occur in LandVilleMain.java
+//
+// If you find yourself needing to do input here rethink your solution.
+
+class LandVille {
+	private int[][] land;
+	// you will need to add the variable 'hasHouse'
+
+	// Constructor...
+	// Assumption - rows and columns has been validated beforehand.
+	// Create the array that the land variable above will reference (using the 'new'
+	// operator)
+	// Initialise the land so that each cell is set to the character '0' (hint: use
+	// one of your
+	// methods!).
+	// Set the value of any other variables
+	public LandVille(int maxRows, int maxColumns) {
+
+	}
+
+	// Display the land grid
+	public void displayLand() {
+
+	}
+
+	// Clear out the land. This involves setting each cell to be the character '0'
+	public void clearLand() {
+
+	}
+
+	// Build a house
+	// Validate the inputs to ensure they make sense for the size of the land
+	// - display an error message and return if they don't.
+
+	// Ensure none of the other rules are violated
+	// - again display an error message and return if they are violated
+
+	// If all is ok "build" the house on the land (using the character '8')
+	// surrounded
+	// by a pretty little fence of '1' characters.
+	// Finally update 'hasHouse' and call on displayLand().
+
+	public void buildHouse(int rows, int columns) {
+
+	}
 
 }
--- src/LandVilleMain.java
+++ src/LandVilleMain.java
@@ -1,8 +1,20 @@
 
+// import whatever packages are needed here
+
 public class LandVilleMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create a Scanner object for input
+
+		// Input # of rows, # of columns for the land size, check for valid input.
+		// If the either input is too small replace it with the smallest feasible size
+		// the land could be.
+
+		// Create an object of class LandVille using the 'new' operator, calling on the
+		// LandVille constructor.
+
+		// Build a loop to display the menu, prompt for input and process it as per
+		// requirements.
 
 	}
 
